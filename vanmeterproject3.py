624efdf77f36bfd0449604bf804cb685ce71d9d7
avanmet1 <55400228+avanmet1@users.noreply.github.com>
2019-10-24 10:47:23 -0400

    Update vanmeterproject3.py

--- vanmeterproject3.py
+++ vanmeterproject3.py
@@ -1,7 +1,14 @@
+source = open('modest.txt')
+
+modest = source.read()
+
+
+
 import re
 
 def book(words):
     if type(words) != str:
         raise TypeError ("Not A String")
 
-result = re.findall('[at]',)
+    result = re.findall('\\w+at\\b', words)
+        
